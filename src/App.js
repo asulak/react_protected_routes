7949a2e10896a7695c03df008cf283a02559e835
asulak <123790407+asulak@users.noreply.github.com>
2023-09-27 19:08:17 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Register from './components/Register';
+import Register from './components/Register';    //User-based permissions 
 import Login from './components/Login';
 import Home from './components/Home';
 import Layout from './components/Layout';
